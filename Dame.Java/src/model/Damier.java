83947eded8858c25472bb4d92f182457ca0b5377
sofiane1234 <79451497+sofiane1234@users.noreply.github.com>
2021-11-17 16:05:15 +0100

    Add files via upload

---
+++ Dame.Java/src/model/Damier.java
@@ -0,0 +1,36 @@
+package model;
+
+// Déclaration de la class Damier
+
+public class Damier {
+	private int damX;
+	private int damY;
+	private char[][] damier;
+	
+	// Construire le damier
+	
+	public Damier(int x, int y) {
+		damX = x;
+		damY = y;
+		damier = new char[damX][damY]; 
+		for (int i = 0; i < damX; i++) {
+			for (int j = 0; j < damY; j++) {
+				damier[i][j] = '0';
+				
+			}
+		}
+	}
+	
+	// Afficher le damier
+	
+	public void printTab() {
+		System.out.println();
+		for (int i = 0; i < damX; i++) {
+			for (int j = 0; j < damY; j++) {
+				System.out.print(" | " + damier[i][j]);
+			}
+			System.out.println(" | ");
+		}
+		System.out.println();
+	}
+}
---
+++ Dame.Java/src/module-info.java
@@ -0,0 +1,2 @@
+module Dame.Java {
+}
